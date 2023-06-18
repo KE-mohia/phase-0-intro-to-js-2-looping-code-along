ce013fe94f59dd3b508976dcc37f7ae2bcc5848d
wayne <keyawayne1@gmail.com>
2023-06-19 00:08:45 +0300

    copmteted assinment

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 //Write yore solution in this file 
-function WriteCards(names, happy, ="happy"){
+function writeCards(names, happy = "happy"){
     let card =[];
-    for (let r = 0; r < names.lenghth; r++){
-        card.push(`Thank  you,${names[r]},for the wonderful ${happy}gift!`);
+    for (let r = 0; r < names.length; r++){
+        card.push(`Thank you, ${names[r]}, for the wonderful ${happy} gift!`);
     }
-console.log(card);
+
 return card;
 }
 function countDown(number){
-    let h=0
-    while(number >=h){
-        console.log(number);
-        number;
+
+
+    for (let index  = number; index >=0 ; index--) {
+        console.log(index)
+        
     }
 }
